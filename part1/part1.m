5dac4b9f3e8dfcd382e0625222f32185bab58e23
henglin <henglin.h@gmail.com>
2017-03-25 15:55:23 +0100

    basic logging function, in progress..

--- part1/part1.m
+++ part1/part1.m
@@ -1,6 +1,9 @@
 % sift_type = 'color';
 sift_type = 'grayscale';
 clusters_number = 400;
+
+svm_param = ['-t -v 3 -c ', num2str(c), ' -g ', num2str(g)];
+
 data_folder = char(strcat('data/', string(sift_type), '/', string(clusters_number), '/'));
 
 mkdir(strcat(data_folder, 'models'));
@@ -22,3 +25,11 @@ run('svm_test_dataset.m');
 
 disp('TESTING');
 run('svm_testing.m');
+
+% store the MAP, and AP for each image category
+ap_data = strcat(num2str(mean_average_precision), ',', num2str(airplanes_ap) ...
+    , ',', num2str(cars_ap), ',', num2str(faces_ap), ',', num2str(motorbikes_ap));
+
+log_data = strcat(num2str(clusters_number), ',', sift_type, ',', ap_data);
+
+write_log(data_folder, log_data);
